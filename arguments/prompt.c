db9292e83afe65b86041cb5969f3bfe943f6c0e4
fizzyfog <idrissisarah@gmail.com>
2023-12-12 16:05:28 +0100

    Remove infinite while loop and change if condition

--- arguments/prompt.c
+++ arguments/prompt.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
-* main - Continuously prompts the user for input and prints the input received
+* main - Prompts the user for input and prints the input received
 *
 * This function prints a "$ " prompt, waits for the user to enter a command,
-* and then prints the entered command on the next line. This process repeats
-* until an end-of-file (EOF) condition is encountered, typically when the user
-* presses Ctrl+D.
+* then prints the entered command on the next line. This process is done once,
+* the program exits after printing the user's input or if an end-of-file (EOF)
+* condition is encountered, typically when the user presses Ctrl+D.
 *
 * Return: 0 on successful execution
 */
@@ -18,18 +18,11 @@ int main(void)
 	size_t len = 0;
 	ssize_t read;
 
-	while (1)
-	{
-		printf("$ ");
-		read = getline(&line, &len, stdin);
+	printf("$ ");
+	read = getline(&line, &len, stdin);
 
-		if (read == -1)
-		{
-			break;
-		}
+	if (read != -1)
 		printf("%s", line);
-		system(line);
-	}
 
 	free(line);
 	return (0);
