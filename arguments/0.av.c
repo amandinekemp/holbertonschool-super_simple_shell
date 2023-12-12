dadae3a24fefed8835415a4c3df1dea5ffda49b4
fizzyfog <idrissisarah@gmail.com>
2023-12-12 15:09:12 +0100

    Auto stash before merge of "main" and "origin/main"

--- arguments/0.av.c
+++ arguments/0.av.c
@@ -1,6 +1,20 @@
-#include <stdio>
+#include <stdio.h>
+
+/**
+* main - prints all the arguments the program receives
+* @ac: number of arguments
+* @av: value of arguments
+*
+* Return: 0
+*/
 
 int main(int ac, char **av)
 {
-	
+	int index;
+
+	for (index = 0; index < ac; index++)
+	{
+		printf("%s\n", av[index]);
+	}
+	return (0);
 }
