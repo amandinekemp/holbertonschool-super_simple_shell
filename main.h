aef627d718eba1f8660241c1fedb3be88b0fe7c4
DDoudine <amandinekemp@gmail.com>
2023-12-14 16:58:44 +0100

    add missing prototypes for all exos

--- main.h
+++ main.h
@@ -8,7 +8,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
 /* System call libraries */
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -16,6 +15,31 @@
 
 /* Function prototypes */
 char **split_string(char *str);
+extern char **environ;
+
+int _setenv(const char *name, const char *value, int overwrite);
+int _unsetenv(const char *name);
+
+void print_path_directories(void);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+char *_strtok(char *str, char *delim);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(char *s1, char *s2, int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+char *_strncat(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _atoi(char *s);
+int _putchar(char c);
+
+
+/* Global variables ? */
 extern char **environ;
 
 #endif /* MAIN_H */
